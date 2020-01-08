6db4eff8b7182b922e575928a642031e014eb6ef
RobinKumar963 <robbhood123@gmail.com>
2020-01-08 11:50:31 +0530

    [RobinKumar] Refactor server.js

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/fundoo-notes'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/fundoo-notes/index.html'));
+res.sendFile(path.join(__dirname+'/fundoo-notes/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
 
 
 
